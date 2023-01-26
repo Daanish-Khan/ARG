80e33f530148b58be65bd512a99a1a379bd0e709
JasonFGC <JasonLFGC@gmail.com>
2023-01-26 16:41:55 -0500

    Testing if image work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import AMOG from './/images/AMOG.png';
 function App() {
   return (
    <div>among balls
-    <image src = {AMOG} className="AMOG" alt=""/>
    </div>
   
   );
 }
-
 export default App;
---
+++ src/Image.js
@@ -0,0 +1,7 @@
+import AMOG from './/images/AMOG.png';
+function Image() {
+    return <img src={AMOG} alt="AMOG" />;
+  }
+
+export default Image;
+  
