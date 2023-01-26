b0e3b92d81723405658eee19bf5774bcf624d85f
Daanish-Khan <dkhan045@uottawa.ca>
2023-01-25 23:20:09 -0500

    Change to react lol

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-document.getElementById('out').innerHTML = "testing";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
