70fdc663c5d3899b7efbecb982e449aace7b0af3
JasonFGC <JasonLFGC@gmail.com>
2023-01-26 16:56:52 -0500

    testing some stuff for text boxes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import AMOG from './/images/AMOG.png';
 function App() {
   return (
    <div>
-    <h1>among balls</h1>;
+    
     <img src={AMOG} alt="AMOG" />
-   </div>
+    <h1>among balls</h1>;
+    <form>
+    <label>
+      Name:
+    <input type="text" name="name" />
+    </label>
+    <input type="submit" value="Submit" />
+    </form>
+    </div>
   
   );
 }
