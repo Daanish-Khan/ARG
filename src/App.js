f7cd92b4962bd3df24a6863406d18f078e16982d
Daanish-Khan <dkhan045@uottawa.ca>
2023-01-26 23:22:20 -0500

    material gworl

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
+import * as React from 'react';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import FormControl from '@mui/material/FormControl';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+
 import AMOG from './/images/AMOG.png';
+
 function App() {
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
    <div>
     
     <img src={AMOG} alt="AMOG" />
     <h1>among balls</h1>
-    <form>
-    <label>
-      Key:
-    <input type="text" name="name" />
-    </label>
-    <input type="Submit" value="sus" />
-    </form>
+    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Key</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
     </div>
   
   );
 }
+
 export default App;
