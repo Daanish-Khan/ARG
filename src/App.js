a494c74ef69f3a792c4e25383643a342efbb88e5
JasonFGC <JasonLFGC@gmail.com>
2023-01-27 12:57:02 -0500

    b

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   };
   
   return (
-   <div style={backgroundcolor="black"}>
+   <div>
     
     <img src={eye} alt="A mysterious eye stares back at you..." className="border d-flex align-items-center justify-content-center"/>
     <Box 
