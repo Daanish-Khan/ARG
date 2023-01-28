5a1b08517563e7b239a6f4df4ff898cfc2bc5000
Daanish-Khan <dkhan045@uottawa.ca>
2023-01-28 01:11:06 -0500

    minecraft font :O

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { Box } from "@mui/material";
+import { Box, createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 import InputField from "./components/Input";
-import ReactAudioPlayer from "react-audio-player";
 
 import eye from "./images/eye.gif";
 import bg from "./components/sounds/bg.mp3";
+import Minecraft from './components/fonts/font.otf'
 
 const fieldColor = "#880808";
 var isDisabled = false;
@@ -16,47 +16,54 @@ function playAudio() {
   document.getElementById("bg").play();
 }
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Minecraft',
+  },
+});
+
 function App() {
   return (
-    
-    <div
-      onMouseOver={playAudio}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-        alignContent: "center",
-        justifyContent: "center",
-      }}
-    >
-      <audio id="bg" loop>
-        <source src={bg} type="audio/mp3"></source>
-      </audio>
+    <ThemeProvider theme={theme}>
       <div
+        onMouseOver={playAudio}
         style={{
           display: "flex",
-          alignItems: "center",
+          flexDirection: "column",
+          height: "100vh",
+          alignContent: "center",
           justifyContent: "center",
         }}
       >
-        <Box
-          component="img"
-          sx={{
-            width: "15%",
+        <audio id="bg" loop>
+          <source src={bg} type="audio/mp3"></source>
+        </audio>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
-          alt="A mysterious eye watches you..."
-          src={eye}
-        />
+        >
+          <Box
+            component="img"
+            sx={{
+              width: "15%",
+            }}
+            alt="A mysterious eye watches you..."
+            src={eye}
+          />
+        </div>
+        <InputField
+          fieldColor={fieldColor}
+          disabledColor={disabledColor}
+          isDisabled={isDisabled}
+          inputWidth="30%"
+          inputFontSize="18"
+          inputText="Key"
+        ></InputField>
       </div>
-      <InputField
-        fieldColor={fieldColor}
-        disabledColor={disabledColor}
-        isDisabled={isDisabled}
-        inputWidth="30%"
-        inputFontSize="18"
-        inputText="Key"
-      ></InputField>
-    </div>
+    </ThemeProvider>
   );
 }
 
