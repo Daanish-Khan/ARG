b3c9f4d161ad963dd24a1a6296763671b7378a86
Daanish-Khan <dkhan045@uottawa.ca>
2023-01-28 01:02:07 -0500

    waiting for something to happen?

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import * as React from "react";
 import { Box } from "@mui/material";
 import InputField from "./components/Input";
+import ReactAudioPlayer from "react-audio-player";
 
 import eye from "./images/eye.gif";
+import bg from "./components/sounds/bg.mp3";
 
 const fieldColor = "#880808";
 var isDisabled = false;
 const disabledColor = "#824343";
 
+function playAudio() {
+  document.getElementById("bg").muted = false;
+  document.getElementById("bg").volume = 0.10;
+  document.getElementById("bg").play();
+}
+
 function App() {
   return (
+    
     <div
+      onMouseOver={playAudio}
       style={{
         display: "flex",
         flexDirection: "column",
@@ -19,6 +29,9 @@ function App() {
         justifyContent: "center",
       }}
     >
+      <audio id="bg" loop>
+        <source src={bg} type="audio/mp3"></source>
+      </audio>
       <div
         style={{
           display: "flex",
